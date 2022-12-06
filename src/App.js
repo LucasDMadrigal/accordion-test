5ab5317322a080fbcb3d092f3a13176ebb87a075
LucasDMadrigal <63689668+LucasDMadrigal@users.noreply.github.com>
2022-12-06 13:13:16 +0000

    funcionalidad accordion completa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
 function App() {
+
+  const [selected, setSelected] = useState(null)
+
+  const data = [
+    {
+      title: "Opcion 1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      title: "Opcion 2",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      title: "Opcion 3",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      title: "Opcion 4",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ]
+
+  const toogle = (i) => {
+    if (selected === i) {
+      return setSelected(null)
+    }
+
+    setSelected(i)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Aprende React pendejo boludo
+        <div className="wrapper">
+          <div className="accordion">
+            {data.map((option, i) => (
+              <div className="item" >
+                <div className="title" onClick={() => toogle(i)}>
+                  <h2>{option.title}</h2>
+                  <span>{selected === i ? '-' : '+'}</span>
+                </div>
+                <div className={`content ${selected === i ? 'show' : ''}`}>{option.content}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
